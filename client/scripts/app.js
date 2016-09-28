810d5f6ccad63822c1602626521ebf7007b6e3a4
clarkdowner <cddowner@gmail.com>
2016-09-28 15:21:13 -0700

    Pass all tests for movie model like property and movie collection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,11 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    if (this.set('like')) {
+      this.set('like', false);
+    } else {
+      this.set('like', true);
+    }
   }
 
 });
@@ -15,13 +19,14 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
